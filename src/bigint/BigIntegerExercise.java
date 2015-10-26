41edb32443b5189f535f5b6ec93c7f51cd0f1fdf
Cyrille Roland AKAMBA <rolandakamba@hotmail.com>
2015-10-27 00:19:42 +0100

    Commit of Marius Work.

--- src/bigint/BigIntegerExercise.java
+++ src/bigint/BigIntegerExercise.java
@@ -1,5 +1,6 @@
 package bigint;
 
+import java.lang.Character.Subset;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
@@ -8,7 +9,7 @@ public class BigIntegerExercise {
 	public BigIntegerExercise() {
 	}
 
-	void expression() {
+	public static void expression() {
 
 		System.out.println("\nExercise 2:");
 		System.out.println("===========\n");
@@ -16,24 +17,42 @@ public class BigIntegerExercise {
 		/************************************************************
 		 * Insert the code of Exercise 2 below this comment!
 		 ************************************************************/
-		System.out.println("test");
-		//llmm
-
-		// bla
+		BigInteger a = new BigInteger("512");
+		BigInteger b = new BigInteger("102");
+		BigInteger c = new BigInteger("3");
+		BigInteger d = new BigInteger("761");
+		BigInteger anobymous = new BigInteger("4");		
+		
+		System.out.println("The result is " + (a.multiply(b).subtract(anobymous)).divide(c).add((d.multiply(d)).subtract(a.subtract(b))));
 
 	}
 
-	void comparison() {
+	public static void comparison() {
 		System.out.println("\nExercise 3:");
 		System.out.println("===========\n");
 
 		/************************************************************
 		 * Insert the code of Exercise 3 below this comment!
 		 ************************************************************/
-
+		BigInteger a = new BigInteger("781");
+		BigInteger b = new BigInteger("12891");
+		
+		if(a.compareTo(b) < 0)
+			System.out.println(a + " is strictly less than " + b);
+		if(a.compareTo(b) <= 0)
+			System.out.println(a + " is less than or equal to " + b);
+		if(	a.compareTo(b) == 0)
+			System.out.println(a + " is equal to " + b);
+		if(	a.compareTo(b) >= 0)
+			System.out.println(a + " is greater than or equal to " + b);
+		if(	a.compareTo(b) > 0)
+			System.out.println(a + " is strictly greater than " + b);
+		if(a.compareTo(b) != 0)
+			System.out.println(a + " is different from " + b);		
+		
 	}
 
-	void randomNumbers() {
+	public static void randomNumbers() {
 		System.out.println("\nExercise 4:");
 		System.out.println("===========\n");
 
@@ -41,20 +60,36 @@ public class BigIntegerExercise {
 		 * Insert the code of Exercise 4 below this comment!
 		 ************************************************************/
 
+		SecureRandom prng = new SecureRandom();
+		BigInteger Num64 = new BigInteger(64, prng);
+		BigInteger Num128 = new BigInteger(128, prng);
+		BigInteger Num256 = new BigInteger(256, prng);
+		BigInteger Num512 = new BigInteger(512, prng);
+		BigInteger Num1024 = new BigInteger(1024, prng);
+		BigInteger Num2048 = new BigInteger(2048, prng);  
+
+		
+		
+		System.out.println("Random 64: " + Num64);
+		System.out.println("Random 128: " + Num128);
+		System.out.println("Random 256: " + Num256);
+		System.out.println("Random 512: " + Num512);
+		System.out.println("Random 1024: " + Num1024);
+		System.out.println("Random 2048: " + Num2048);
 	}
 
 	public void run() {
 		/*
 		 * Uncomment the method after the implementation!
 		 */
-		//expression();
-		//comparison();
-		//randomNumbers();
+		expression();
+		comparison();
+		randomNumbers();
 	}
 
 	public static void main(String[] args) {
 		BigIntegerExercise m = new BigIntegerExercise();
-
+		
 		m.run();
 	}
 
